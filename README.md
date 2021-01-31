0c2988110592d676227e8b77c6c995d7a0a2b5bf
Christian Stigen Larsen <csl@csl.name>
2021-01-31 15:21:55 +0100

    Adds code and README

---
+++ README.md
@@ -0,0 +1,16 @@
+Utregning av kommunale avgifter i Sandnes kommune.
+===
+
+Dette er for en ordinær bolig på 107 m² uten vannmåler som har egne
+avfallsdunker.
+
+Satsene er for 2021. Kan ikke garantere at det stemmer, men basert på fjoråret
+så tror jeg dette skal være riktig.
+
+For å kjøre:
+
+    $ python3 komm.py
+
+Du kan endre variabelen `kvm` for ditt tilfelle for å beregne pris.
+
+Fra i fjor ser det ut som den totale avgiften for meg har økt med 11%.
---
+++ komm.py
@@ -0,0 +1,46 @@
+"""
+kommunale avgifter per 2021
+
+https://www.sandnes.kommune.no/teknisk-og-eiendom/vann-avlop-og-renovasjon/kommunale-avgifter/
+"""
+
+# størrelse bolig
+kvm = 107
+moms = 1.25
+
+# 2021: uten vannmåler
+# priser tatt (uten moms) fra nettsiden over
+post = {
+    "forbruk vann": kvm * 11.00,
+    "forbruk avløp": kvm * 16.60,
+    "abbonnement vann": kvm * 4.20,
+    "abbonnement avløp": kvm * 7.60,
+    "grunngebyr": 785 / moms,
+    "restavfall 240l": 2826 / moms,
+    "papiravfall": 393 / moms,
+    "bioavfall": 1178 / moms,
+}
+
+# Under stemmer god med 2020 og regningen jeg fikk
+post_2020 = {
+    "forbruk vann": kvm * 9,
+    "forbruk avløp": kvm * 16,
+    "abbonnement vann": kvm * 4.20,
+    "abbonnement avløp": kvm * 7.60,
+    "grunngebyr": 600,
+    "restavfall 240l": 1890,
+    "papiravfall": 300,
+    "bioavfall": 840,
+}
+
+total = sum(post.values())
+post["total per år"] = total
+post["total per halvår"] = total / 2
+
+width = max(len(x) for x in post.keys())
+
+for navn, pris in post.items():
+    if navn == "total per år":
+        print("=" * (width + 9))
+    print("%-*s" % (width, navn.capitalize()),
+          "%8.2f" % (pris * moms))
